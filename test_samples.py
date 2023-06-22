78bde447c19af82b33f24a52db4cd04c6b95b5b8
cammil81 <50446152+cammil81@users.noreply.github.com>
2023-06-22 17:14:41 +0200

    Update test_samples.py

--- test_samples.py
+++ test_samples.py
@@ -1,23 +1,9 @@
 import streamlit as st
-from datetime import time, datetime
+import pandas as pd
+import numpy as np
 
-st.header("st.slider")
-st.subheader("Slider")
+st.header("Line chart")
 
-age = st.slider("How old are you?", 0, 130, 25)
-st.write("I'm ", age, "years old")
-                
-st.subheader("Range Slider")
-values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
-st.write("Values: ",values)
-             
-st.subheader("Range time slider")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-appointment = st.slider("Schedule your appointment:", value=(time(11, 30),time(12, 45)),format="hh:mm")
-st.write("You're scheduled for: ", appointment)
-
-st.subheader("Datetime slider")
-start_time=st.slider("When do you start?", value=datetime(2023, 1, 1, 9, 30),format="MM/DD/YY - hh:mm")
-
-st.write("Start time:", start_time)
-                     
+st.line_chart(ch
