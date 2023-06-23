ae1adfd7ca43bd872748be87f6a85d88ba3b4d9d
cammil81 <50446152+cammil81@users.noreply.github.com>
2023-06-23 15:45:14 +0200

    Update test_samples.py

--- test_samples.py
+++ test_samples.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.header("st.selectbox")
 
 option = st.selectbox(
-  "What is your favourite color?, ("Blue", "Red", "Green"))
+  "What is your favourite color?", ("Blue", "Red", "Green"))
 
 st.write("your favourite colir is ", option)
