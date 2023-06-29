d6de6cf447f543aa9ed712131a99be1c654130f1
cammil81 <50446152+cammil81@users.noreply.github.com>
2023-06-29 16:07:29 +0200

    Update test_samples.py

--- test_samples.py
+++ test_samples.py
@@ -3,9 +3,9 @@ import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-st.header("streamlit_pandas_profiling")
+st.header('`streamlit_pandas_profiling`')
 
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
 pr = df.profile_report()
 st_profile_report(pr)
